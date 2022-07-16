44bc3d43d7d89119723a0043b2f0f9b4c02c1ecd
Humberto Bravo <humbandroid@gmail.com>
2022-07-15 21:59:28 -0300

    adicionando mais funcoes. //3

--- apptest/lib/main.dart
+++ apptest/lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:apptest/second.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Second());
 }
 
 class MyApp extends StatelessWidget {
---
+++ apptest/lib/second.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Second extends StatelessWidget {
+  const Second({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Test App',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: const Page(),
+    );
+  }
+}
+
+class Page extends StatelessWidget {
+  const Page({Key? key}) : super(key: key);
+  
+  get behavior => null;
+  
+  get child => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Page'),
+      ),
+      body: const Center(
+        child: Text('This is the second page'),
+      ),
+    );
+  }
+}
